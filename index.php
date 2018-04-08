7a4e15869f43a2782b64d299bd426e7c48682a71
Derek <derek@dadian-smith.net>
2018-04-08 15:06:03 -0500

    Add font

--- index.php
+++ index.php
@@ -3,12 +3,13 @@
 <head>
   <meta charset="utf-8">
   <title>The Flow Gallery | Rules to Play By</title>
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
   <link rel="stylesheet" href="css/default.css">
 </head>
 <body>
   <header>
+    <h1>Rules to Play By</h1>
     <nav>
-
     </nav>
   </header>
 
