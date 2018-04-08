0bd2a5954c4bdb079d49e3fbcc530c58478f6a28
Derek <derek@dadian-smith.net>
2018-04-08 15:02:19 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>The Flow Gallery | Rules to Play By</title>
+  <link rel="stylesheet" href="css/default.css">
+</head>
+<body>
+  <header>
+    <nav>
+
+    </nav>
+  </header>
+
+  <main>
+    <img src="img/flux-card-blank.jpg" alt="">
+  </main>
+</body>
+</html>
