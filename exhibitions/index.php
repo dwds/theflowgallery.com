da5962e94c9a70112b8a393a655cde1ad0236a9f
Derek <derek@dadian-smith.net>
2018-04-08 23:59:08 -0500

    Make exhibitions page

---
+++ exhibitions/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <title>Exhibitions | The Flow Gallery</title>
+  <link href="https://fonts.googleapis.com/css?family=Roboto:500" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Dosis:200,600" rel="stylesheet">
+  <link rel="stylesheet" href="../css/default.css">
+</head>
+<body>
+  <header>
+    <a class="site-title" href="../">The Flow Gallery</a>
+    <nav>
+      <ul>
+        <li><a href=".">exhibitions</a></li>
+        <li><a href="../about">about</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <main class="exhibitions">
+    <h1>exhibitions</h1>
+
+    <p><strong>* currently accepting submissions *</strong></p>
+
+    <h2>Rules to Play By — call for entries</h2>
+
+    <p class="submit-link"><b><a href="https://docs.google.com/forms/d/e/1FAIpQLScZhUcSIYOKSLhe6Fix0pMjwmaeETV5KjvBKs-2NO7YtFHgQA/viewform">Submit online</a></b> (requires Google sign-in)</p>
+
+    <p><strong>Deadline for submissions: Monday April 30, 2018.</strong></p>
+
+    <p><b>Rules to Play By</b> is a playful weekend exhibition of rules, instructions, recipes, scripts, programs, scores or any art exploring the theme of “rules to play by.”</p>
+
+    <p>We invite you to submit anything from a tabletop RPG to a musical score. All mediums are welcomed.</p>
+
+    <p>Selected pieces will be displayed in up to two locations for the weekend of <b>July 27–29, 2018</b>. One location is a popup art gallery. The other is within Pixelpop Festival. Both locations are in <b>St. Louis, MO</b>.</p>
+
+    <p>If selected you will receive a notice of where your work will be showing.</p>
+
+    <p>Selected work will also be compiled or photographed to be included in a show <b>catalog</b> that will be made available as a free pdf.</p>
+
+    <p>Please note that artists are responsible for delivery and pickup of all work. Further information will be sent out to selected artists.</p>
+
+    <p class="submit-link"><b><a href="https://docs.google.com/forms/d/e/1FAIpQLScZhUcSIYOKSLhe6Fix0pMjwmaeETV5KjvBKs-2NO7YtFHgQA/viewform">Submit online</a></b> (requires Google sign-in)</p>
+
+    <h2>Timeline</h2>
+    <p>
+      4/30 - Submission deadline<br>
+      5/10 - Notice of acceptance<br>
+      7/23 - Work must be delivered<br>
+      7/27 - Opening night
+    </p>
+
+    <h2>Submitting your work</h2>
+    <p>Provide a text description and, if applicable, visual documentation (digital images, videos, or web pages). If the work cannot be documented visually, please provide a clear description of the piece and how it would be presented.</p>
+
+    <p>You can either upload files directly through <b><a href="https://docs.google.com/forms/d/e/1FAIpQLScZhUcSIYOKSLhe6Fix0pMjwmaeETV5KjvBKs-2NO7YtFHgQA/viewform">this form</a></b>, or provide links to your project online. If you have any questions please contact <a href="mailto:smithderekw@gmail.com">smithderekw@gmail.com</a> or <a href="mailto:brianna@thebricade.com">brianna@thebricade.com</a>.</p>
+  </main>
+</body>
+</html>
