9da8be9af65b2cf770519afec0b5bd549102d390
Derek <derek@dadian-smith.net>
2018-04-08 19:14:53 -0500

    Add meta viewport

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>The Flow Gallery | Rules to Play By</title>
   <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
   <link rel="stylesheet" href="css/default.css">
