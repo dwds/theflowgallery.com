5eef7a58a44c9cd1b3382c15ccae778d0da99597
Derek <derek@dadian-smith.net>
2018-04-08 23:34:11 -0500

    Make about page

---
+++ about/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <title>About | The Flow Gallery</title>
+  <link href="https://fonts.googleapis.com/css?family=Roboto:500" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Dosis:200,600" rel="stylesheet">
+  <link rel="stylesheet" href="../css/default.css">
+</head>
+<body>
+  <header>
+    <a class="site-title" href="../">The Flow Gallery</a>
+    <nav>
+      <ul>
+        <li><a href="../exhibitions/">exhibitions</a></li>
+        <li><a href=".">about</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <main class="about">
+    <h1>about</h1>
+
+    <p>The Flow Gallery is a series of exhibitions exploring the intersection of art &amp; games. As its name suggests, the gallery does not exist in a single location. We exhibit at game festivals and art galleries in an attempt to bridge game designers and artists. The Flow Gallery is curated by Brianna Shuttleworth &amp; Derek Dadian-Smith.</p>
+
+    <section class="bios">
+      <article class="bio">
+        <img class="brianna-img" src="../img/brianna-shuttleworth.jpg" alt="Brianna Shuttleworth">
+        <h2>Brianna Shuttleworth</h2>
+        <ul>
+          <li><a href="https://www.thebricade.com/">thebricade.com</a></li>
+          <li><a href="https://twitter.com/TheBriCade">@TheBriCade</a></li>
+        </ul>
+      </article>
+
+      <article class="bio">
+        <img class="derek-img" src="../img/derek-dadian-smith.jpg" alt="Derek Dadian-Smith">
+        <h2>Derek Dadian-Smith</h2>
+        <ul>
+          <li><a href="https://derekds.com">derekds.com</a></li>
+          <li><a href="https://twitter.com/dwds_">@dwds_</a></li>
+        </ul>
+
+      </article>
+    </section>
+  </main>
+</body>
+</html>
