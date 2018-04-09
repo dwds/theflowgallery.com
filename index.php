bd26af15949bdea3509fb5404c16cf40aaa52b4c
Derek <derek@dadian-smith.net>
2018-04-08 22:00:04 -0500

    Update google fonts link

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>The Flow Gallery | Rules to Play By</title>
-  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:500" rel="stylesheet">
   <link rel="stylesheet" href="css/default.css">
 </head>
 <body>
