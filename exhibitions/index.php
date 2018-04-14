3d279f665f1575740d47bae346b235ee2408e4ef
Derek <derek@dadian-smith.net>
2018-04-14 16:41:30 -0500

    Update CSS version number

--- exhibitions/index.php
+++ exhibitions/index.php
@@ -26,7 +26,7 @@
   <meta property="og:image:alt" content="A paper card in the style of Fluxus event scores. The text “submit” is repeated.">
 
   <title>Exhibitions | The Flow Gallery</title>
-  <!-- <link href="https://fonts.googleapis.com/css?family=Dosis:200,600|Roboto:500" rel="stylesheet">  -->
+
   <style>
     /* latin */
     @font-face {
@@ -45,7 +45,7 @@
     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
     }
   </style>
-  <link rel="stylesheet" href="../css/default.css?v=1.4">
+  <link rel="stylesheet" href="../css/default.css?v=1.5">
 </head>
 <body>
   <header>
