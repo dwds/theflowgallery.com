4b00814ee839dfed051bd2fc4ac30458ae0604f5
Derek <derek@dadian-smith.net>
2018-05-02 11:16:20 -0500

    Close submissions on exhibition page

--- exhibitions/index.php
+++ exhibitions/index.php
@@ -61,49 +61,21 @@
   <main class="exhibitions">
     <h1>exhibitions</h1>
 
-    <p><strong>* currently accepting submissions *</strong></p>
+    <h2>Rules to Play By</h2>
 
-    <h2>Rules to Play By — call for entries</h2>
+    <p>July 27–29, 2018 @ St. Louis, MO, USA</p>
 
-    <p class="submit-link"><b><a href="https://docs.google.com/forms/d/e/1FAIpQLScZhUcSIYOKSLhe6Fix0pMjwmaeETV5KjvBKs-2NO7YtFHgQA/viewform">Submit online</a></b> (requires Google sign-in)</p>
-
-    <p><strong>Deadline for submissions: Monday April 30, 2018.</strong></p>
+    <p><strong>Submissions are now closed. Notice of acceptance will be sent by May 10, 2018.</strong></p>
 
     <p><b>Rules to Play By</b> is a playful weekend exhibition of rules, instructions, recipes, scripts, programs, scores or any art exploring the theme of “rules to play by.”</p>
 
     <p>We are particularly interested in the point where instruction art becomes game-like, or where game rules intersect conceptual art.</p>
 
-    <p>We invite you to submit anything from a tabletop RPG to a musical score. All media are welcomed.</p>
-
-    <h3>Looking for inspiration?</h3>
-
-    <p>Here are some examples of work that have inspired us while putting together this exhibition.</p>
-
-    <ul>
-      <li><a href="http://www.a-i-u.net/instructions.html">Yoko Ono, Instruction Paintings</a></li>
-      <li><a href="https://www.moma.org/collection/works/portfolios/126322?locale=it">George Brecht, Water Yam</a></li>
-      <li>and <a href="http://www.deluxxe.com/beat/fluxusworkbook.pdf">other</a> Fluxus <a href="https://www.moma.org/explore/inside_out/2012/12/21/exhibiting-fluxus-keeping-score-in-tokyo-1955-1970-a-new-avant-garde/">scores</a></li>
-      <li><a href="https://en.wikipedia.org/wiki/Surrealist_techniques">Surrealist games</a></li>
-      <li><a href="http://modernistcommons.ca/islandora/object/yale%3A352">Tristan Tzara, Recipe for a Dadaist Poem</a></li>
-      <li><a href="http://www.mocp.org/detail.php?type=related&kv=3219&t=people">John Baldessari, Throwing Three Balls in the Air to Get a Straight Line</a></li>
-    </ul>
-
-    <h2>Submitting your work</h2>
-    <p>Provide a text description and, if applicable, visual documentation (digital images, videos, or web pages). If the work cannot be documented visually, please provide a clear description of the piece and how it would be presented.</p>
-
-    <p>You can either upload files directly through <b><a href="https://docs.google.com/forms/d/e/1FAIpQLScZhUcSIYOKSLhe6Fix0pMjwmaeETV5KjvBKs-2NO7YtFHgQA/viewform">this form</a></b>, or provide links to your project online. If you have any questions please contact <a href="mailto:smithderekw@gmail.com">smithderekw@gmail.com</a> or <a href="mailto:brianna@thebricade.com">brianna@thebricade.com</a>.</p>
-
-
-    <h2>Exhibition Details</h2>
     <p>Selected pieces will be displayed in up to two locations for the weekend of <b>July 27–29, 2018</b>. One location is a popup art gallery. The other is within <b><a href="http://pixelpopfestival.com/">Pixelpop Festival</a></b>. Both locations are in <b>St. Louis, MO</b>.</p>
-
-    <p>If selected you will receive a notice of where your work will be showing.</p>
 
     <p>Selected work will also be compiled or photographed to be included in a show <b>catalog</b> that will be made available as a free pdf.</p>
 
-    <p>Please note that artists are responsible for delivery and pickup of all work. Further information will be sent out to selected artists.</p>
-
-    <p class="submit-link"><b><a href="https://docs.google.com/forms/d/e/1FAIpQLScZhUcSIYOKSLhe6Fix0pMjwmaeETV5KjvBKs-2NO7YtFHgQA/viewform">Submit online</a></b> (requires Google sign-in)</p>
+    <!-- <p class="submit-link"><b><a href="https://docs.google.com/forms/d/e/1FAIpQLScZhUcSIYOKSLhe6Fix0pMjwmaeETV5KjvBKs-2NO7YtFHgQA/viewform">Submit online</a></b> (requires Google sign-in)</p> -->
 
     <h2>Timeline</h2>
     <p>
@@ -112,6 +84,42 @@
       7/23 - Work must be delivered<br>
       7/27 - Opening night
     </p>
+
+    <div class="old-info" style="opacity: 0.4;">
+      <h2>Call for entries</h2>
+
+      <p><strong>*Submissions are now closed.*</strong></p>
+
+      <p><strong>Notice of acceptance will be sent by May 10, 2018.</strong></p>
+      <!-- <p class="submit-link"><b><a href="https://docs.google.com/forms/d/e/1FAIpQLScZhUcSIYOKSLhe6Fix0pMjwmaeETV5KjvBKs-2NO7YtFHgQA/viewform">Submit online</a></b> (requires Google sign-in)</p> -->
+
+      <p><strong>Deadline for submissions: Monday April 30, 2018.</strong></p>
+
+      <p>We invite you to submit anything from a tabletop RPG to a musical score. All media are welcome.</p>
+
+      <h3>Looking for inspiration?</h3>
+
+      <p>Here are some examples of work that have inspired us while putting together this exhibition.</p>
+
+      <ul>
+        <li><a href="http://www.a-i-u.net/instructions.html">Yoko Ono, Instruction Paintings</a></li>
+        <li><a href="https://www.moma.org/collection/works/portfolios/126322?locale=it">George Brecht, Water Yam</a></li>
+        <li>and <a href="http://www.deluxxe.com/beat/fluxusworkbook.pdf">other</a> Fluxus <a href="https://www.moma.org/explore/inside_out/2012/12/21/exhibiting-fluxus-keeping-score-in-tokyo-1955-1970-a-new-avant-garde/">scores</a></li>
+        <li><a href="https://en.wikipedia.org/wiki/Surrealist_techniques">Surrealist games</a></li>
+        <li><a href="http://modernistcommons.ca/islandora/object/yale%3A352">Tristan Tzara, Recipe for a Dadaist Poem</a></li>
+        <li><a href="http://www.mocp.org/detail.php?type=related&kv=3219&t=people">John Baldessari, Throwing Three Balls in the Air to Get a Straight Line</a></li>
+      </ul>
+
+      <h2>Submitting your work</h2>
+      <p>Provide a text description and, if applicable, visual documentation (digital images, videos, or web pages). If the work cannot be documented visually, please provide a clear description of the piece and how it would be presented.</p>
+
+      <p>Please note that artists are responsible for delivery and pickup of all work. Further information will be sent out to selected artists.</p>
+
+      <p>You can either upload files directly through <b><a href="https://docs.google.com/forms/d/e/1FAIpQLScZhUcSIYOKSLhe6Fix0pMjwmaeETV5KjvBKs-2NO7YtFHgQA/viewform">this form</a></b>, or provide links to your project online. If you have any questions please contact <a href="mailto:smithderekw@gmail.com">smithderekw@gmail.com</a> or <a href="mailto:brianna@thebricade.com">brianna@thebricade.com</a>.</p>
+
+      <p>If selected you will receive a notice of where your work will be showing.</p>
+
+    </div>
   </main>
 </body>
 </html>
