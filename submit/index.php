c3e05a33c4147f2baff4ef67839ac3c03d7a50f9
Derek <derek@dadian-smith.net>
2019-06-03 16:59:44 -0500

    Create submit section

---
+++ submit/index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-101882975-2"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-101882975-2');
+  </script>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+
+  <meta name="description" content="Currently accepting submissions for Rules to Play By, an exhibition of rules, instructions, recipes, scripts, programs, and scores.">
+
+  <!-- Twitter card -->
+  <meta name="twitter:card" content="summary_large_image">
+  <!-- <meta name="twitter:site" content="@theflowgallery"> -->
+  <meta property="og:url" content="https://theflowgallery.com/">
+  <meta property="og:title" content="Rules to Play By: A game / art exhibition">
+  <meta property="og:description" content="July 27–29 in STL, MO. An exhibition of rules, instructions, recipes, scripts, programs, and scores exploring the theme of “rules to play by.”">
+  <meta property="og:image" content="http://theflowgallery.com/img/rules-to-play-by.png">
+  <meta property="og:image:alt" content="A paper card in the style of Fluxus event scores. The text “submit” is repeated.">
+
+  <title>Exhibitions | The Flow Gallery</title>
+
+  <style>
+    /* latin */
+    @font-face {
+    font-family: 'Dosis';
+    font-style: normal;
+    font-weight: 200;
+    src: local('Dosis ExtraLight'), local('Dosis-ExtraLight'), url(https://fonts.gstatic.com/s/dosis/v7/HhyXU5sn9vOmLzGnKuCFMIs.woff2) format('woff2');
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+    }
+    /* latin */
+    @font-face {
+    font-family: 'Dosis';
+    font-style: normal;
+    font-weight: 600;
+    src: local('Dosis SemiBold'), local('Dosis-SemiBold'), url(https://fonts.gstatic.com/s/dosis/v7/HhyXU5sn9vOmLzG3L-CFMIs.woff2) format('woff2');
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+    }
+  </style>
+  <link rel="stylesheet" href="../css/default.css?v=2.1">
+</head>
+<body>
+  <header>
+    <a class="site-title" href="../">The Flow Gallery</a>
+    <nav>
+      <ul>
+        <li><a href=".">exhibitions</a></li>
+        <li><a href="../about">about</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <main class="exhibitions">
+    <h1>exhibitions</h1>
+
+    <h2>Rules to Play By</h2>
+
+    <p>July 27–29, 2018 @ St. Louis, MO, USA</p>
+
+    <p><strong>Opening reception</strong> Friday July 27, 7:00pm @ <b><a href="https://www.snapdragonstl.com/">Snapdragon Studio</a></b>.</p>
+    <p><strong>Gallery hours</strong> Sat/Sun July 28–29, 3:00pm–7:00pm @ <b><a href="https://www.snapdragonstl.com/">Snapdragon Studio</a></b></p>
+    <p>Also @ <b><a href="http://pixelpopfestival.com/">PixelPop Festival</a></b> July 28–29, 10:00am–6:00pm</p>
+
+    <p>Participatory performance of Frans Van Lent's <cite>A Performative Walk 3</cite>:</p>
+      <ul>
+        <li>Friday July 27, 6:00pm @ Snapdragon Studio</li>
+        <li>Sunday July 29, 9:00am @ PixelPop Festival</li>
+      </ul>
+
+    <p><b>Rules to Play By</b> is a playful weekend exhibition of rules, instructions, recipes, scripts, programs, and scores exploring the theme of “rules to play by.”</p>
+
+    <p>We are particularly interested in the point where instruction art becomes game-like, or where game rules intersect conceptual art.</p>
+
+    <p>Selected pieces will be displayed in two locations for the weekend of <b>July 27–29, 2018</b>. One location is <b><a href="https://www.snapdragonstl.com/">Snapdragon Studio</a></b>, a florist studio converted into a gallery during the summer. The other is within <b><a href="http://pixelpopfestival.com/">PixelPop Festival</a></b>. Both locations are in <b>St. Louis, MO</b>.</p>
+
+    <h3>Featuring work by</h3>
+
+    <ul class="artist-list">
+      <li><a href="https://sharangbiswas.myportfolio.com/">Sharang Biswas</a></li>
+      <li><a href="https://tambalaya.itch.io/">Tamara Duplantis</a></li>
+      <li><a href="http://www.jesseengland.net">Jesse England</a></li>
+      <li><a href="https://www.pninax.com/">Maya Felixbrodt</a></li>
+      <li><a href="http://hollygramazio.net">Holly Gramazio</a></li>
+      <li><a href="http://www.harrygiles.org">Harry Giles</a></li>
+      <li><a href="http://fransvanlent.nl/">Frans van Lent</a></li>
+      <li><a href="https://lotharioaps.com/">Lothario A. Peon de Schuyter</a></li>
+      <li><a href="http://www.r25th.com">Adam Schroeder</a></li>
+      <li><a href="http://www.robertspahr.com">Robert Spahr</a></li>
+      <li><a href="http://www.daniellewilliamson.com">Dani Williamson</a></li>
+      <li><a href="https://jingyufanclub.com/">Jing Yu</a></li>
+    </ul>
+
+    <p>Selected work will also be compiled or photographed to be included in a show <b>catalog</b> that will be made available as a free pdf.</p>
+
+    <!-- <p class="submit-link"><b><a href="https://docs.google.com/forms/d/e/1FAIpQLScZhUcSIYOKSLhe6Fix0pMjwmaeETV5KjvBKs-2NO7YtFHgQA/viewform">Submit online</a></b> (requires Google sign-in)</p> -->
+
+    <div class="old-info" style="opacity: 0.4;">
+      <h2>Call for entries</h2>
+
+      <p><strong>*Submissions are now closed.*</strong></p>
+
+      <p><strong>Notice of acceptance will be sent by May 10, 2018.</strong></p>
+      <!-- <p class="submit-link"><b><a href="https://docs.google.com/forms/d/e/1FAIpQLScZhUcSIYOKSLhe6Fix0pMjwmaeETV5KjvBKs-2NO7YtFHgQA/viewform">Submit online</a></b> (requires Google sign-in)</p> -->
+
+      <p><strong>Deadline for submissions: Monday April 30, 2018.</strong></p>
+
+      <p>We invite you to submit anything from a tabletop RPG to a musical score. All media are welcome.</p>
+
+      <h3>Looking for inspiration?</h3>
+
+      <p>Here are some examples of work that have inspired us while putting together this exhibition.</p>
+
+      <ul>
+        <li><a href="http://www.a-i-u.net/instructions.html">Yoko Ono, Instruction Paintings</a></li>
+        <li><a href="https://www.moma.org/collection/works/portfolios/126322?locale=it">George Brecht, Water Yam</a></li>
+        <li>and <a href="http://www.deluxxe.com/beat/fluxusworkbook.pdf">other</a> Fluxus <a href="https://www.moma.org/explore/inside_out/2012/12/21/exhibiting-fluxus-keeping-score-in-tokyo-1955-1970-a-new-avant-garde/">scores</a></li>
+        <li><a href="https://en.wikipedia.org/wiki/Surrealist_techniques">Surrealist games</a></li>
+        <li><a href="http://modernistcommons.ca/islandora/object/yale%3A352">Tristan Tzara, Recipe for a Dadaist Poem</a></li>
+        <li><a href="http://www.mocp.org/detail.php?type=related&kv=3219&t=people">John Baldessari, Throwing Three Balls in the Air to Get a Straight Line</a></li>
+      </ul>
+
+      <h2>Submitting your work</h2>
+      <p>Provide a text description and, if applicable, visual documentation (digital images, videos, or web pages). If the work cannot be documented visually, please provide a clear description of the piece and how it would be presented.</p>
+
+      <p>Please note that artists are responsible for delivery and pickup of all work. Further information will be sent out to selected artists.</p>
+
+      <p>If you have any questions please contact <a href="mailto:smithderekw@gmail.com">smithderekw@gmail.com</a> or <a href="mailto:brianna@thebricade.com">brianna@thebricade.com</a>.</p>
+
+      <p>If selected you will receive a notice of where your work will be showing.</p>
+
+    </div>
+  </main>
+</body>
+</html>
